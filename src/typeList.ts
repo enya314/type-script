15b67f1e556f75414d0042b6bd2448c2b51c6613
enya314 <103580466+enya314@users.noreply.github.com>
2022-04-27 22:38:12 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "hoge";
+
+// array
+let arr1: Array<number> = [1, 2, 3];
+let arr2: number[] = [4, 5, 6];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "aaaa" };
